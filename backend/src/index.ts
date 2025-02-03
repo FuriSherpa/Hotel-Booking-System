9faebe48da6d15298a4df8d3a333afcc0c4ddf35
Furi Sherpa <furisherpa403@gmail.com>
2025-02-03 20:28:40 +0545

    Registration authentication logic added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,19 @@
 import express, {Request, Response} from 'express';
 import cors from 'cors';
 import "dotenv/config";
+import mongoose from 'mongoose'
+import userRoutes from './routes/users';
+import authRoutes from './routes/auth';
+
+mongoose.connect(process.env.MONGODB_COLLECTION_STRING as string)
 
 const app = express(); // creating express app
 app.use(express.json()) // converts the body of api into json automatically
 app.use(express.urlencoded({extended: true})) // parse the url to get parameters
 app.use(cors()) // for security
 
-
-app.get("/api/test", async (req: Request, res: Response)=> {
-    res.json({message: "hello from express endpoint!"});
-});
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(7000, ()=> {
     console.log("Server running on localhost 7000");
