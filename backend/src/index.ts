06bd8bb35b7f513b8d159a31cc293966a344237a
Furi Sherpa <furisherpa403@gmail.com>
2025-02-08 23:26:10 +0545

    Login authorization added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,13 +4,18 @@ import "dotenv/config";
 import mongoose from 'mongoose'
 import userRoutes from './routes/users';
 import authRoutes from './routes/auth';
+import cookieParser from "cookie-parser";
 
 mongoose.connect(process.env.MONGODB_COLLECTION_STRING as string)
 
 const app = express(); // creating express app
+app.use(cookieParser());
 app.use(express.json()) // converts the body of api into json automatically
 app.use(express.urlencoded({extended: true})) // parse the url to get parameters
-app.use(cors()) // for security
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+})); // for security
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
